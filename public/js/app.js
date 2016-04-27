1b2429f0b5a44b7068291cd5f72dfdff2bb52ec0
Aishwarya <aishwaryashilpi5395@gmail.com>
2016-04-27 11:33:46 +0530

    add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,22 @@
 var socket = io();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
+console.log(name+' wants to join '+room);
 
 socket.on('connect', function (){
-	console.log('Connected to socket.io server');
+	console.log('Connected to socket.io server');	
 });
 
 socket.on('message', function (message){
-	var momentTimestamp = moment.utc(message.timestamp);	
+	var momentTimestamp = moment.utc(message.timestamp);
+	var $message = jQuery('.messages');	
+
 	console.log('New message: ');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p>'+ momentTimestamp.local().format('h:mm a')+ ': ' + message.text +'</p>');
+	$message.append('<p><strong>'+ message.name + ' ' +momentTimestamp.local().format('h:mm a')+ ': </strong></p>');
+	$message.append('<p>'+ message.text +'</p>');
 });
 
 //Handles submitting of a new message
@@ -19,6 +26,7 @@ $form.on('submit', function (event){
 	event.preventDefault();
 
 	socket.emit('message', {
+		name: name,
 		text: $form.find('input[name=message]').val()
 	});
 
